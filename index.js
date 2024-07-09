1fc2a7ef0a5f63365f1a511c8de730a4fd79c844
GamerYuan <f2pplayer0801@gmail.com>
2024-07-09 22:41:01 +0800

    feat: add dotenv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { setupSdk } from "dissonity";
 window.addEventListener("DOMContentLoaded", () => {
 
   setupSdk({
-    clientId: 1/*your-app-id*/,
+    clientId: process.env.CLIENT_ID /*your-app-id*/,
     scope: ["rpc.voice.read", "guilds.members.read"],
     tokenRoute: "/api/token"
   });
